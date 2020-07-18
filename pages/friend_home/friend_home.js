bc13f8733d182f71d59a272a872ae4530dca2164
李军 <17600993295@163.com>
2020-07-18 14:31:08 +0800

    他的艺术馆页面

--- pages/friend_home/friend_home.js
+++ pages/friend_home/friend_home.js
@@ -4,14 +4,20 @@ const app = getApp();
 let that;
 Page({
 	data: {
-		visitId: '',
+    visitId: '',
+    avatar: '',
+    nickname: '',
+    level: '',
+    donate: '',
+    worksList: []
 	},
 	onLoad: function (options) {
 		that = this;
 		if (options.visitId == undefined) {
 			wx.navigateBack();
 		}
-		console.log(options.visitId, 'options.visitId')
+    console.log(options.visitId, 'options.visitId')
+    this.onHandleOptions(options)
 		this.setData({
 			visitId: options.visitId
 		});
@@ -25,7 +31,15 @@ Page({
 				that.init();
 			};
 		}
-	},
+  },
+  onHandleOptions(options) {
+    this.setData({
+      avatar: options.avatar,
+      nickname: options.nickname,
+      level: options.level,
+      donate: options.donate
+    })
+  },
 	onReady: function () {
 	},
 	onShow: function () {
@@ -40,10 +54,11 @@ Page({
 			url: '/works/list',
 			method: "POST",
 			data: {
-        userId: this.data.user.data.id,
-        visitId: "1283642307770343424" || that.data.visitId
+        visitId: this.data.user.data.id,
+        userId: "1283642307770343424" || that.data.visitId
 			}
 		}).then((data) => {
+      this.setData({worksList: data.data})
       console.log(data, "data");
 			// for (let index = 0; index < data.data.length; index++) {
 			// 	if (data.data[index].id == that.data.wobg_id) {
@@ -53,5 +68,10 @@ Page({
 			// 	}
 			// }
 		})
-	},
+  },
+  gowork() {
+    wx.navigateTo({
+      url: '/pages/work/work?user_id=' + this.data.user.data.id
+    })
+  }
 })
